c450c10ee0663cab04af5537e2029e7712e20f9b
CoderYangWu <745296707@qq.com>
2021-02-09 23:43:35 +0800

    首次提交

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-Header></home-Header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
