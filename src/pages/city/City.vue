8c716aa95e199f8289f7bf3653a190d735a62e10
CoderYangWu <745296707@qq.com>
2021-02-11 01:19:35 +0800

    Alphabet

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,15 +2,19 @@
 <div>
  <city-header></city-header>
  <city-search></city-search>
+ <city-list></city-list>
+ <city-alphabet></city-alphabet>
 </div>
 </template>
 
 <script>
+import CityAlphabet from './components/Alphabet.vue'
 import CityHeader from './components/Header.vue'
+import CityList from './components/List.vue'
 import CitySearch from './components/Search.vue'
 
 export default {
-  components: { CityHeader, CitySearch },
+  components: { CityHeader, CitySearch, CityList, CityAlphabet },
     name: 'City'
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+      .list
+        display flex
+        flex-direction column
+        justify-content center
+        position absolute
+        list-style none
+        top 1.58rem
+        right 0
+        bottom 0
+        width .4rem
+        .item
+            line-height .4rem
+            text-align center
+            color $bgColor
+</style>
