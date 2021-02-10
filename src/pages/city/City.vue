c854735320f9d7fa0b182c376d8e42745a86e9bd
CoderYangWu <745296707@qq.com>
2021-02-11 00:12:29 +0800

    city

---
+++ src/pages/city/City.vue
@@ -0,0 +1,16 @@
+<template>
+<city-header></city-header>
+</template>
+
+<script>
+import CityHeader from '../city/components/Header.vue'
+
+export default {
+  components: { CityHeader },
+    name: 'City'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题</div>
-    <div class="header-right">
+    <router-link to='/city'>
+     <div class="header-right">
       {{this.city}}
       <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+     </div>
+    </router-link>
   </div>
 </template>
 
