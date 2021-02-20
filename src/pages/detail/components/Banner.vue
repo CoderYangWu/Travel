102ffceb2def3f8b779acfefeb456db4fec13722
CoderYangWu <745296707@qq.com>
2021-02-20 23:23:50 +0800

    axios

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,30 +1,37 @@
 <template>
   <div>
   <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg">
+      <img class="banner-img" :src="bannerImg">
       <div class="banner-info">
-          <div class="banner-tittle">大连圣亚海洋世界(AAAA景区)</div>
-          <div class="banner-number"><span class="iconfont">&#xe692;</span>36</div>
+          <div class="banner-tittle">{{this.sightName}}</div>
+          <div class="banner-number"><span class="iconfont">&#xe692;</span>
+          {{this.bannerImgs.length}}</div>
       </div>
       </div>
-    <common-gallary :imgs="imgs" v-show="showGallary"
-    @close="handleGallaryClose"></common-gallary>
+      <fade-animation>
+        <common-gallary :imgs="bannerImgs" v-show="showGallary"
+        @close="handleGallaryClose"></common-gallary>
+      </fade-animation>
+
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary.vue'
-
+import FadeAnimation from 'common/fade/FadeAnimation.vue'
 export default {
   name:'DetailBanner',
+  props:{
+    sightName:String,
+    bannerImg:String,
+    bannerImgs:Array
+  },
   data() {
     return{
       showGallary: false,
-      imgs:['//img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
-             'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_350x240_8d02892d.png']
     }
   },
-  components: {CommonGallary},
+  components: {CommonGallary,FadeAnimation},
   methods: {
     handleBannerClick() {
       this.showGallary = true
