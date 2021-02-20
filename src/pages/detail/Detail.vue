5b175b7f98c91853657e39f0f950cb689fa104da
CoderYangWu <745296707@qq.com>
2021-02-20 21:19:40 +0800

    header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,24 @@
 <template>
- <detail-banner></detail-banner>  
+<div>
+   <detail-banner></detail-banner>  
+   <detail-header></detail-header>
+   <div class="content"></div>
+</div>
 </template>
 
 <script>
+import DetailHeader from './components/Header.vue'
 import DetailBanner from './components/Banner.vue'
 
 export default {
     name:'Detail',
-  components: { DetailBanner },
+  components: { DetailBanner, DetailHeader },
 
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height: 50rem
+    
 </style>
